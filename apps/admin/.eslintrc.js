ba467131af2258cdc4c69389246ecc0102e66dd7
truongan <truonganfi@gmail.com>
2023-11-17 15:42:45 +0700

    some touch to complete convert repo to turborepo

--- apps/admin/.eslintrc.js
+++ apps/admin/.eslintrc.js
@@ -2,9 +2,6 @@ const { join } = require("node:path");
 
 module.exports = {
   extends: require("eslint-config-custom/next"),
-  rules: {
-    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-  },
   parserOptions: {
     project: join(__dirname, "tsconfig.json"),
   },
