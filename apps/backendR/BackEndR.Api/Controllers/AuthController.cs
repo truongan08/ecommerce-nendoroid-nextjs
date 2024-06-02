8a6e0ecf8c1615f164bf8ed3eb4dfc687c43bc5a
truongan <truonganfi@gmail.com>
2024-06-02 22:05:23 +0700

    create AuthController val

--- apps/backendR/BackEndR.Api/Controllers/AuthController.cs
+++ apps/backendR/BackEndR.Api/Controllers/AuthController.cs
@@ -1,4 +1,17 @@
 namespace server.Controllers;
 public class AuthController : BaseController {
-    
+   private readonly UserManager<User> userManager;
+    private readonly RoleManager<IdentityRole> roleManager;
+    private readonly IConfiguration _configuration;
+
+    public AuthController(
+        UserManager<User> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IConfiguration configuration
+    )
+    {
+        this.userManager = userManager;
+        this.roleManager = roleManager;
+        _configuration = configuration;
+    }
 }
