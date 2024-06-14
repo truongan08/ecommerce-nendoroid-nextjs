316684d90cd06206af7755fec1785ac8f8a8278e
truongan <truonganfi@gmail.com>
2024-06-14 19:31:16 +0700

    product

---
+++ server/BackEndR.Api/Models/Product.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndR.Api.Models
+{
+﻿using System.ComponentModel.DataAnnotations;
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Brand { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Slug { get; set; }
+    public string? Desc { get; set; }
+    public string? Color { get; set; }
+    public int TotalQuantity { get; set; }
+
+    public decimal CurrentPrice { get; set; }
+    public decimal? DiscountPrice { get; set; }
+
+    public string Date { get; set; }
+
+    public List<ProductImage>? Images { get; set; }
+    public List<Favorite>? Favorites { get; set; }
+    public List<ProductVariant>? ProductVariants { get; set; }
+
+    [Required]
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+
+    [Required]
+    public int SubcategoryId { get; set; }
+    public Subcategory Subcategory { get; set; }
+}
+}
