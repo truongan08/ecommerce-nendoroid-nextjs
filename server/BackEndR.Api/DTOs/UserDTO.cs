06dd437cc6fd6493115484e6df410070cd0af8df
truongan <truonganfi@gmail.com>
2024-06-17 22:45:31 +0700

    user DTO

---
+++ server/BackEndR.Api/DTOs/UserDTO.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEndR.Api.DTOs
+{
+{
+    public class UserDTO
+    {
+        [Required]
+        public string UserID { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+        public AddressDTO? Address { get; set; }
+    }
+}
+}
